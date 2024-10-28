0fead144ccdbb288f53f31fad457921e7134653d
EllaHanson <ellahanson.126@gmail.com>
2024-10-27 20:08:34 -0700

    user bug fix

--- src/api/recipe.py
+++ src/api/recipe.py
@@ -12,20 +12,26 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
-class user(BaseModel):
+class User(BaseModel):
     name: str
     email: str
     password: str
 
-@router.post("/post")
-def post_recipe(new_user: user):
-
-
+@router.post("/post/user")
+def add_user(new_user: User):
     with db.engine.begin() as connection:
         print(f"insert user {new_user.name}")
-        connection.execute(sqlalchemy.text("INSERT INTO users (username, email, password) VALUES :new_name, :new_email, :new_password"), 
+        connection.execute(sqlalchemy.text("INSERT INTO users (username, email, password) VALUES (:new_name, :new_email, :new_password)"), 
                            {"new_name": new_user.name, "new_email": new_user.email, "new_password": new_user.password})
     return "OK"
+"""
+@router.post("/loggin")
+def logg_in_out(user_id: int):
+     with db.engine.begin() as connection:
+            connection.execute(sqlalchemy.text(""))
+
+        """
+
 
 
 
