6081cca18aa8ec351f158a41983fb70682eb367a
katieslobodsky <katieslobodsky@gmail.com>
2024-11-07 07:53:29 -0800

    favorites update

--- src/api/favorites.py
+++ src/api/favorites.py
@@ -5,13 +5,20 @@ from . import auth
 import sqlalchemy
 from src import database as db
 
-
 router = APIRouter(
     prefix="/favorite",
     tags=["favorite"],
     dependencies=[Depends(auth.get_api_key)],
 )
 
-@router.post("/post/favorite")
-def post_favorite():
+@router.post("/explore/favorites")
+def add_favorite(user_id, recipe_id):
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("INSERT INTO favorites (user_id, recipe_id) VALUES (:user_id, :recipe_id)"), {"user_id": user_id, "recipe_id": recipe_id})
     return "OK"
+
+@router.get("/blog/favorites")
+def get_favorites(user_id):
+    with db.engine.begin() as connection:
+        favorites = connection.execute(sqlalchemy.text("SELECT FROM favorites WHERE user_id = :user_id"), {"user_id": user_id})
+    return favorites
