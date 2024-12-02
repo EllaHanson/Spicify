a474e3a355b63f9a43972f66672c04c3763749c6
jeman618 <jcisne23@calpoly.edu>
2024-12-02 10:13:28 -0800

    checks for recipe, user, and combines SQL statements

--- src/api/rating.py
+++ src/api/rating.py
@@ -31,9 +31,8 @@ def post_rating(recipe_id: str, rating: int):
     recipe_rating_sql = """UPDATE recipes SET rating = 
                             ROUND((CAST(rating as numeric) * rating_count + :rating)/ (rating_count + 1), 1),
                             rating_count = rating_count + 1
-                            WHERE recipe_id = :recipe_id"""
-    
-    recipe_name_sql = "SELECT title FROM recipes WHERE recipe_id = :recipe_id"
+                            WHERE recipe_id = :recipe_id
+                            RETURNING title"""
 
     # This addresses the peer review feedback where rating boundaries could be broken
     if rating > 5 or rating < 0:
@@ -41,8 +40,7 @@ def post_rating(recipe_id: str, rating: int):
     else:
         with db.engine.begin() as connection:
             recipe_rating = connection.execute(sqlalchemy.text(recipe_rating_sql), 
-                                            {"rating": rating, "recipe_id": recipe_id})
-            recipe_name = connection.execute(sqlalchemy.text(recipe_name_sql), {"recipe_id": recipe_id}).scalar()
+                                            {"rating": rating, "recipe_id": recipe_id}).scalar()
 
-        print(f"\"{recipe_name}\" has been given a rating of {rating} / 5")
+        print(f"\"{recipe_rating}\" has been given a rating of {rating} / 5")
     return "Rating posted successfully!"
