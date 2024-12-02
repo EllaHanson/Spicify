fc051be3654e1bd8f45b615983ed0877e54ff7d2
jeman618 <jcisne23@calpoly.edu>
2024-12-02 12:10:51 -0800

    More format changes

--- src/api/ratings.py
+++ src/api/ratings.py
@@ -19,12 +19,6 @@ class Rating(BaseModel):
 @router.post("/post/rating")
 def post_rating(recipe_id: int, rating: int):
 
-   with db.engine.begin() as connection:
-        in_table = connection.execute(sqlalchemy.text("SELECT COUNT(*) FROM recipes WHERE recipe_id = :id"), {"id": recipe_id}).fetchone().count
-
-        if not in_table:
-            return HTTPException(status_code = 400, detail = "Recipe id does not exist!")
-
     # calculates the new rating by: (overall rating * amount of ratings + recent rating) / (amount of ratings + 1)
     recipe_rating_sql = """UPDATE recipes SET rating = 
                             ROUND((CAST(rating as numeric) * rating_count + :rating)/ (rating_count + 1), 1),
@@ -32,11 +26,15 @@ def post_rating(recipe_id: int, rating: int):
                             WHERE recipe_id = :recipe_id
                             RETURNING title"""
 
-    # This addresses the peer review feedback where rating boundaries could be broken
-    if rating > 5 or rating < 0:
-        raise HTTPException(status_code = 400, detail = "Given rating breaks the boundaries!")
-    else:
-        with db.engine.begin() as connection:
+    with db.engine.begin() as connection:
+        # Checks if recipe id exists
+        in_table = connection.execute(sqlalchemy.text("SELECT COUNT(*) FROM recipes WHERE recipe_id = :id"), {"id": recipe_id}).fetchone().count
+        if not in_table:
+                return HTTPException(status_code = 400, detail = "Recipe id does not exist!")
+        # This addresses the peer review feedback where rating boundaries could be broken
+        elif rating > 5 or rating < 0:
+            raise HTTPException(status_code = 400, detail = "Given rating breaks the boundaries!")
+        else:
             recipe_rating = connection.execute(sqlalchemy.text(recipe_rating_sql), 
                                             {"rating": rating, "recipe_id": recipe_id}).scalar()
 
