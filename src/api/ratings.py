c332851256dc04a7d9fd5439e012ba479551f3c3
jeman618 <jcisne23@calpoly.edu>
2024-12-02 10:57:54 -0800

    Format changes

--- src/api/ratings.py
+++ src/api/ratings.py
@@ -19,25 +19,27 @@ class Rating(BaseModel):
 @router.post("/post/rating")
 def post_rating(recipe_id: int, rating: int):
 
-    with db.engine.begin() as connection:
+   with db.engine.begin() as connection:
         in_table = connection.execute(sqlalchemy.text("SELECT COUNT(*) FROM recipes WHERE recipe_id = :id"), {"id": recipe_id}).fetchone().count
 
         if not in_table:
             return HTTPException(status_code = 400, detail = "Recipe id does not exist!")
 
-        # calculates the new rating by: (overall rating * amount of ratings + recent rating) / (amount of ratings + 1)
-        recipe_rating_sql = """UPDATE recipes SET rating = 
-                                ROUND((CAST(rating as numeric) * rating_count + :rating)/ (rating_count + 1), 1),
-                                rating_count = rating_count + 1
-                                WHERE recipe_id = :recipe_id
-                                RETURNING title"""
+    # calculates the new rating by: (overall rating * amount of ratings + recent rating) / (amount of ratings + 1)
+    recipe_rating_sql = """UPDATE recipes SET rating = 
+                            ROUND((CAST(rating as numeric) * rating_count + :rating)/ (rating_count + 1), 1),
+                            rating_count = rating_count + 1
+                            WHERE recipe_id = :recipe_id
+                            RETURNING title"""
 
-        # This addresses the peer review feedback where rating boundaries could be broken
-        if rating > 5 or rating < 0:
-            raise HTTPException(status_code = 400, detail = "Given rating breaks the boundaries!")
-        else:
+    # This addresses the peer review feedback where rating boundaries could be broken
+    if rating > 5 or rating < 0:
+        raise HTTPException(status_code = 400, detail = "Given rating breaks the boundaries!")
+    else:
+        with db.engine.begin() as connection:
             recipe_rating = connection.execute(sqlalchemy.text(recipe_rating_sql), 
-                                                {"rating": rating, "recipe_id": recipe_id}).scalar()
+                                            {"rating": rating, "recipe_id": recipe_id}).scalar()
 
     print(f"\"{recipe_rating}\" has been given a rating of {rating} / 5")
     return Response(status_code = 200)
+
